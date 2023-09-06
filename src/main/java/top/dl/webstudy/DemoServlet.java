295a29c78f64631027049a703b0b0122bdf3f02d
minderers <2210151384@qq.com>
2023-09-06 12:29:58 +0800

    JavaWeb学习项目初始化

---
+++ src/main/java/top/dl/webstudy/DemoServlet.java
@@ -0,0 +1,12 @@
+package top.dl.webstudy;
+
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * @author: minder
+ * @createTime: 2023/09/06 11:09
+ * @description:
+ **/
+public class DemoServlet extends HttpServlet {
+
+}
